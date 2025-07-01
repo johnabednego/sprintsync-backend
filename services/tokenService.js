90e9f4696e6446496efbdd1e7a1af211eb5c525f
johnabednego <jilimajohnabednego@gmail.com>
2025-07-01 18:19:56 +0000

    added isAdmin to the toekn payload

--- services/tokenService.js
+++ services/tokenService.js
@@ -1,6 +1,6 @@
 const jwt = require('jsonwebtoken');
 
 exports.generateAccessToken = user =>
-  jwt.sign({ id: user.id, role: user.role, sub: user.id }, process.env.JWT_SECRET, { expiresIn: '1h' });
+  jwt.sign({ id: user.id, role: user.role, sub: user.id, isAdmin: user.isAdmin }, process.env.JWT_SECRET, { expiresIn: '1h' });
 
 exports.verifyToken = token => jwt.verify(token, process.env.JWT_SECRET);
